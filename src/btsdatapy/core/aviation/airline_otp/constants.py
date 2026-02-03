151728235ecddfbcdb32c7e624798dcb4e6dcfe4
Adam Lynch <adamclynch03@gmail.com>
2026-02-02 21:58:31 -0600

    Updated package structure

---
+++ src/btsdatapy/core/aviation/airline_otp/constants.py
@@ -0,0 +1,7 @@
+from btsdatapy.core.utils.obfuscation import rot13
+
+REPORTING_CARRIER_OTP_TABLE_ID: str = "FGJ"
+REPORTING_CARRIER_OTP_SH146_NAME: str = rot13("o0-time")
+
+MARKETING_CARRIER_OTP_TABLE_ID: str = "FGK"
+MARKETING_CARRIER_OTP_SH146_NAME: str = rot13("o0-time")
--- src/btsdatapy/core/aviation/airline_otp/models.py
+++ src/btsdatapy/core/aviation/airline_otp/models.py
@@ -1,11 +1,4 @@
 from btsdatapy.core.models import BtsTableRequestPayload
-from btsdatapy.core.utils.obfuscation import rot13
-
-REPORTING_CARRIER_OTP_TABLE_ID: str = "FGJ"
-REPORTING_CARRIER_OTP_SH146_NAME: str = rot13("o0-time")
-
-MARKETING_CARRIER_OTP_TABLE_ID: str = "FGK"
-MARKETING_CARRIER_OTP_SH146_NAME: str = rot13("o0-time")
 
 
 class ReportingCarrierOTPPayload(BtsTableRequestPayload):
